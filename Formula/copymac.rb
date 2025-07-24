53ce4817cd9a74219ce17d1a59bcbcff84b219e8
Alex <alexc@myofer.co.il>
2025-07-24 14:48:57 +0300

    Update formula to use hosted tar.gz in tap repo

--- Formula/copymac.rb
+++ Formula/copymac.rb
@@ -1,6 +1,6 @@
 class Copymac < Formula
   desc "macOS clipboard CLI manager"
-  homepage "https://github.com/alciu10/CopyMac-Repo"
+  homepage "https://github.com/alciu10/homebrew-copymac"
   url "https://github.com/alciu10/homebrew-copymac/releases/download/v1.0.0/copymac.tar.gz"
   sha256 "76433ad2a4538590017cb30375ff06cf91b6481fe6fcf7e7aae4b8fbc62b4a7b"
   version "1.0.0"
