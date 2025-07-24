529e997546c5eb3d29771538dc30532fc472c95e
Alex <alexc@myofer.co.il>
2025-07-24 14:38:11 +0300

    Update formula to install CLI binary

--- Formula/copymac.rb
+++ Formula/copymac.rb
@@ -1,19 +1,15 @@
 class Copymac < Formula
-  desc "macOS clipboard manager"
+  desc "macOS clipboard CLI manager"
   homepage "https://github.com/alciu10/CopyMac-Repo"
   url "https://github.com/alciu10/CopyMac-Repo/releases/download/v1.0.0/copymac.tar.gz"
-  sha256 "1eef0596bdc8dfda0edec554119078c263c1a34200913e6ed164bc9c0f0c67e6"
+  sha256 "76433ad2a4538590017cb30375ff06cf91b6481fe6fcf7e7aae4b8fbc62b4a7b"
   version "1.0.0"
 
   def install
-    prefix.install "CopyMac.app"
-    bin.write_exec_script "#{prefix}/CopyMac.app/Contents/MacOS/CopyMac"
+    bin.install "copymac-clipboard"
   end
 
-  def caveats
-    <<~EOS
-      To launch CopyMac:
-        open -a "#{prefix}/CopyMac.app"
-    EOS
+  test do
+    system "#{bin}/copymac-clipboard", "--help"
   end
 end
