13024b523ac4bea0d288b4e7ca6c079ff590d87e
Alex <alexc@myofer.co.il>
2025-08-03 20:39:06 +0300

    Update cask to v1.3.1 - working hotkeys release

--- Casks/copymac.rb
+++ Casks/copymac.rb
@@ -1,10 +1,10 @@
 cask "copymac" do
-  version "1.3.0"
-  sha256 "a877224271b29323acd501b18de1a52d27689f2a21837376d5c7fe288e6de151"
+  version "1.3.1"
+  sha256 "015540fc8ed610f3cfa252333136ed697d8450ac9cd1a105b6a87c70ac4cdd23"
 
   url "https://github.com/alciu10/homebrew-copymac/releases/download/v#{version}/CopyMac.app.zip"
   name "CopyMac"
-  desc "macOS clipboard manager"
+  desc "macOS clipboard manager with global hotkey support"
   homepage "https://github.com/alciu10/homebrew-copymac"
 
   app "CopyMac.app"
