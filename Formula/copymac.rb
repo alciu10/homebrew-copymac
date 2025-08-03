fadc715e3a66ebe833b89a16ed7ac97505da82d4
Alex <alexc@myofer.co.il>
2025-08-03 16:35:34 +0300

    Update CopyMac formula to v1.3.0

--- Formula/copymac.rb
+++ Formula/copymac.rb
@@ -1,15 +1,15 @@
 class Copymac < Formula
-  desc "macOS clipboard GUI manager with smooth animations and favorites"
+  desc "macOS clipboard CLI manager"
   homepage "https://github.com/alciu10/homebrew-copymac"
-  url "https://github.com/alciu10/homebrew-copymac/releases/download/v1.2.1/CopyMac.app.zip"
-  sha256 "119e1df12efa6afbb1f7bb0daf621d9720f46eaae4f27e4cbdbdbe0865985fbb"
-  version "1.2.1"
+  url "https://github.com/alciu10/CopyMac-Repo/releases/download/v1.3.0/copymac.tar.gz"
+  sha256 "4f5a7d64465182621bfa7435ff32475c665649aac416898bdeab3037943e947f"
+  version "1.3.0"
 
   def install
-    bin.install "copymac"
+    bin.install "copymac-clipboard"
   end
 
   test do
-    system "#{bin}/copymac", "--help"
+    system "#{bin}/copymac-clipboard", "--help"
   end
 end
