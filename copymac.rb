6c140ef8c1352fbbbf7c52b4172472b387fbdb50
Alex <alexc@myofer.co.il>
2025-08-03 14:51:19 +0300

    Update CopyMac to v1.2.1

--- copymac.rb
+++ copymac.rb
@@ -1,9 +1,9 @@
 class Copymac < Formula
   desc "Clipboard manager app for macOS"
-  homepage "https://github.com/alciu10/CopyMac-Repo"
-  url "https://github.com/alciu10/CopyMac-Repo/releases/download/v1.0.0/copymac.tar.gz"
-  sha256 "1eef0596bdc8dfda0edec554119078c263c1a34200913e6ed164bc9c0f0c67e6"
-  version "1.0.0"
+  homepage "https://github.com/alciu10/homebrew-copymac"
+  url "https://github.com/alciu10/homebrew-copymac/releases/download/v1.2.1/CopyMac.app.zip"
+  sha256 "PASTE_THE_SHA256_HERE"
+  version "1.2.1"
 
   def install
     bin.install "copymac"
