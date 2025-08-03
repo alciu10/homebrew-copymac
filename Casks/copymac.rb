478921ebec36d5cdd8ba5258d5f3dbca31b45363
Alex <alexc@myofer.co.il>
2025-08-03 20:58:03 +0300

    Update to v1.3.2 - working hotkeys release

--- Casks/copymac.rb
+++ Casks/copymac.rb
@@ -1,11 +1,17 @@
 cask "copymac" do
-  version "1.3.1"
-  sha256 "015540fc8ed610f3cfa252333136ed697d8450ac9cd1a105b6a87c70ac4cdd23"
+  version "1.3.2"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
   url "https://github.com/alciu10/homebrew-copymac/releases/download/v#{version}/CopyMac.app.zip"
   name "CopyMac"
-  desc "macOS clipboard manager with global hotkey support"
+  desc "macOS clipboard manager with working global hotkeys"
   homepage "https://github.com/alciu10/homebrew-copymac"
 
   app "CopyMac.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-r", "-d", "com.apple.quarantine", "#{appdir}/CopyMac.app"],
+                   sudo: false
+  end
 end
