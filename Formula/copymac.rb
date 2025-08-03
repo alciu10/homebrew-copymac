ab1fbc1986e3ac6053937ee77cc5326f3f629bde
Alex <alexc@myofer.co.il>
2025-08-03 16:44:58 +0300

    Update CopyMac formula to v1.3.0 with new SHA

--- Formula/copymac.rb
+++ Formula/copymac.rb
@@ -2,7 +2,7 @@ class Copymac < Formula
   desc "macOS clipboard CLI manager"
   homepage "https://github.com/alciu10/homebrew-copymac"
   url "https://github.com/alciu10/CopyMac-Repo/releases/download/v1.3.0/copymac.tar.gz"
-  sha256 "48055275b03793f6682a0348ea34998dd381c0a39d59a4b297457e753c8a80fd"
+  sha256 "8ddcd57f800663e52a1afcc5a2832bd8c36ef4ccd9d6f5800057ad68205e912a"
   version "1.3.0"
 
   def install
