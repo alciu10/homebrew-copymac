b0bb9a686318211e948f5b6a0c7c4e0aaa999af5
Alex <alexc@myofer.co.il>
2025-08-03 18:28:14 +0300

    Update cask to use v1.2.1 from correct repository

--- Casks/copymac.rb
+++ Casks/copymac.rb
@@ -1,11 +1,11 @@
 cask "copymac" do
-  version "1.3.0"
-  sha256 "322b95efa0922720547a81a27ea7d05107d0d381cd22ec6e3c2103ce7aed58f4"
+  version "1.2.1"
+  sha256 "119e1df12efa6afbb1f7bb0daf621d9720f46eaae4f27e4cbdbdbe0865985fbb"
 
-  url "https://github.com/alciu10/CopyMac-Repo/releases/download/v#{version}/CopyMac.app.zip"
+  url "https://github.com/alciu10/homebrew-copymac/releases/download/v#{version}/CopyMac.app.zip"
   name "CopyMac"
   desc "macOS clipboard manager"
-  homepage "https://github.com/alciu10/CopyMac-Repo"
+  homepage "https://github.com/alciu10/homebrew-copymac"
 
   app "CopyMac.app"
 end
