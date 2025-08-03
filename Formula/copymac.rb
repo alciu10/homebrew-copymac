c42c726eb1193d02752b218044b03d731a8038e1
Alex <alexc@myofer.co.il>
2025-08-03 14:58:39 +0300

    Update Formula to use v1.2.1 pre-built binary

--- Formula/copymac.rb
+++ Formula/copymac.rb
@@ -1,18 +1,15 @@
 class Copymac < Formula
   desc "macOS clipboard GUI manager with smooth animations and favorites"
-  homepage "https://github.com/alciu10/CopyMac-Repo"
-  url "https://github.com/alciu10/CopyMac-Repo/archive/af200f52cf5a86778f73781695375e4e2abefd3e.tar.gz"
-  sha256 "14bba0fdb2c9b5b292218c46bbdd3feb2215391df97b4e80db042af76ae0c8fd"
-  version "1.1.0"
-
-  depends_on xcode: ["12.0", :build]
+  homepage "https://github.com/alciu10/homebrew-copymac"
+  url "https://github.com/alciu10/homebrew-copymac/releases/download/v1.2.1/CopyMac.app.zip"
+  sha256 "119e1df12efa6afbb1f7bb0daf621d9720f46eaae4f27e4cbdbdbe0865985fbb"
+  version "1.2.1"
 
   def install
-    system "swift", "build", "--configuration", "release", "--disable-sandbox"
-    bin.install ".build/release/copymac-clipboard"
+    bin.install "copymac"
   end
 
   test do
-    system "true"  # Simple test that always passes
+    system "#{bin}/copymac", "--help"
   end
 end
