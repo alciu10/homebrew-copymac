6ca540c0db4a01772102ae5a82392a44df821e3f
Alex <alexc@myofer.co.il>
2025-08-03 20:05:50 +0300

    Update cask SHA256 for fixed v1.3.0

--- Casks/copymac.rb
+++ Casks/copymac.rb
@@ -1,6 +1,6 @@
 cask "copymac" do
   version "1.3.0"
-  sha256 "228dd5167a6d6b17122851b841c112f5db9628e77d680c417f5b5a3e91ea97b3"
+  sha256 "a877224271b29323acd501b18de1a52d27689f2a21837376d5c7fe288e6de151"
 
   url "https://github.com/alciu10/homebrew-copymac/releases/download/v#{version}/CopyMac.app.zip"
   name "CopyMac"
