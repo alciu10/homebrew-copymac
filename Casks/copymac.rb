0d46419083d7a3567a7d4c6f9ac5f0203cec2c10
Alex <alexc@myofer.co.il>
2025-08-03 19:58:38 +0300

    Update cask SHA256 for clean v1.3.0 zip

--- Casks/copymac.rb
+++ Casks/copymac.rb
@@ -1,6 +1,6 @@
 cask "copymac" do
   version "1.3.0"
-  sha256 "322b95efa0922720547a81a27ea7d05107d0d381cd22ec6e3c2103ce7aed58f4"
+  sha256 "228dd5167a6d6b17122851b841c112f5db9628e77d680c417f5b5a3e91ea97b3"
 
   url "https://github.com/alciu10/homebrew-copymac/releases/download/v#{version}/CopyMac.app.zip"
   name "CopyMac"
