d7ea2a4be37699d69f4c3b69e95da985ecbbe8af
Alex <alexc@myofer.co.il>
2025-08-04 15:35:38 +0300

    Update CopyMac to v1.3.6

--- Formula/copymac.rb
+++ Formula/copymac.rb
@@ -1,9 +1,9 @@
 class Copymac < Formula
   desc "Clipboard manager for macOS"
   homepage "https://github.com/alciu10/homebrew-copymac"
-  url "https://github.com/alciu10/homebrew-copymac/releases/download/v1.3.3/copymac-1.3.3.zip"
-  sha256 "4bded454fd9be4fc7c4f41fdd5e817de0b315576e586420c72b204276f382d22"
-  version "1.3.3"
+  url "https://github.com/alciu10/homebrew-copymac/releases/download/v1.3.6/copymac-1.3.6.zip"
+  sha256 "ef9352c3ed2e5f693a13fe8a2b8c73b13c934e4f4a6960fda1410598308ccd2d"
+  version "1.3.6"
   license "MIT"
 
   def install
