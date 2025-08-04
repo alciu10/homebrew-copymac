1aed7b082c2a00e2eadad09786d42af8b3e482fd
Alex <alexc@myofer.co.il>
2025-08-04 13:51:13 +0300

    Fix formula to use binary from homebrew-copymac repo

--- Formula/copymac.rb
+++ Formula/copymac.rb
@@ -1,7 +1,7 @@
 class Copymac < Formula
   desc "Clipboard manager for macOS"
-  homepage "https://github.com/alciu10/CopyMac"
-  url "https://github.com/alciu10/CopyMac/releases/download/v1.3.3/copymac-1.3.3.zip"
+  homepage "https://github.com/alciu10/homebrew-copymac"
+  url "https://github.com/alciu10/homebrew-copymac/releases/download/v1.3.3/copymac-1.3.3.zip"
   sha256 "fcfe6ae860d4921f65c16732c1e803de759565dbf09f01bbb41895e7c18a337d"
   version "1.3.3"
   license "MIT"
