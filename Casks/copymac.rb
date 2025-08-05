497ac7dae0133c4e3639e8e09cc742346dc32ea1
Alex <alexc@myofer.co.il>
2025-08-05 08:13:06 +0300

    Fix cask to use v1.4.0 instead of v1.3.2

--- Casks/copymac.rb
+++ Casks/copymac.rb
@@ -2,7 +2,7 @@ cask "copymac" do
   version "1.4.0"
   sha256 "ac77d05de036529831d385779df6dc23cb65d7ab22134873c6fa224c4ac17018"
 
-  url "https://github.com/alciu10/homebrew-copymac/releases/download/v#{version}/copymac-#{version}.zip"
+  url "https://github.com/alciu10/homebrew-copymac/releases/download/v1.4.0/copymac-1.4.0.zip"
   name "CopyMac"
   desc "Clipboard manager for macOS"
   homepage "https://github.com/alciu10/homebrew-copymac"
