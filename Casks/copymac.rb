0332b82a1ab13702aa001bf9e1e875033fb707b9
Alex <alexc@myofer.co.il>
2025-08-05 08:09:32 +0300

    Add Homebrew cask for GUI app installation

--- Casks/copymac.rb
+++ Casks/copymac.rb
@@ -1,17 +1,11 @@
 cask "copymac" do
-  version "1.3.2"
-  sha256 "22a280289dc791646cd162045a3fb414392994469c4f78b7546c3e3d87034278"
+  version "1.4.0"
+  sha256 "ac77d05de036529831d385779df6dc23cb65d7ab22134873c6fa224c4ac17018"
 
-  url "https://github.com/alciu10/homebrew-copymac/releases/download/v#{version}/CopyMac.app.zip"
+  url "https://github.com/alciu10/homebrew-copymac/releases/download/v#{version}/copymac-#{version}.zip"
   name "CopyMac"
-  desc "macOS clipboard manager with working global hotkeys"
+  desc "Clipboard manager for macOS"
   homepage "https://github.com/alciu10/homebrew-copymac"
 
   app "CopyMac.app"
-
-  postflight do
-    system_command "/usr/bin/xattr",
-                   args: ["-r", "-d", "com.apple.quarantine", "#{appdir}/CopyMac.app"],
-                   sudo: false
-  end
 end
