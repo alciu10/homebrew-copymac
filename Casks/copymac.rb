475e56a2b03a48a21a67a3e3d617bbac7988fe12
Alex <alexc@myofer.co.il>
2025-08-09 21:01:31 +0300

    Update Homebrew cask to version 1.5.1

--- Casks/copymac.rb
+++ Casks/copymac.rb
@@ -1,6 +1,6 @@
 cask "copymac" do
-  version "1.5.0"
-  sha256 "0ed313217ff7382561df2a9c4e1e3bec8e605c3c0a8bb6a6af40af310468e226"
+  version "1.5.1"
+  sha256 "bcec883488c2adfd292ab199d92f0141c505132c98878c7cf48598164c75f31f"
 
   url "https://github.com/alciu10/homebrew-copymac/releases/download/v#{version}/copymac-#{version}.zip"
   name "CopyMac"
